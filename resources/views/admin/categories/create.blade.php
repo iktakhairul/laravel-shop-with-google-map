c592d795617847d6477846363145a8b3b029dbb2
iktakhairul <iktakhairul@gmail.com>
2021-05-19 13:24:09 +0600

    create category view fixes

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -3,22 +3,20 @@
 
     <div class="card">
         <div class="card-header">
-            {{ trans('Edit') }} {{ trans('Category Title') }}
+            {{ trans('Create') }} {{ trans('Category') }}
         </div>
 
         <div class="card-body">
-            <form method="POST" action="{{ route("admin.categories.update", [$category->id]) }}" enctype="multipart/form-data">
-                @method('PUT')
+            <form method="POST" action="{{ route("admin.categories.store") }}" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
-                    <label class="required" for="name">{{ trans('Category Name') }}</label>
-                    <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $category->name) }}" required>
+                    <label class="required" for="name">{{ trans('Name') }}</label>
+                    <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
                     @if($errors->has('name'))
                         <div class="invalid-feedback">
                             {{ $errors->first('name') }}
                         </div>
                     @endif
-                    <span class="help-block">{{ trans('Category Fields') }}</span>
                 </div>
                 <div class="form-group">
                     <button class="btn btn-danger" type="submit">
