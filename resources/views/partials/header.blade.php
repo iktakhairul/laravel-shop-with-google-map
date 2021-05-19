8321f619100f68eef5e97dba4b421d14ff7442aa
iktakhairul <iktakhairul@gmail.com>
2021-05-19 13:37:50 +0600

    home header fixes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,28 +1,206 @@
 
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-<header id="site-header" class="site-header" role="banner" style="background-color: #292929">
-    <div class="container">
-        <div class="site-logo-wrap">
-            <hgroup>
-                <h1 class='site-title site-title-no-desc'> <a  style="color:#FFFFFF;" href='{{ route('home') }}' title='{{ config('app.name', 'Laravel Shops') }}' rel='home'>{{ config('app.name', 'Laravel Shops') }}</a></h1>
-            </hgroup>
-        </div>
-        <nav id="primary-nav" class="primary-nav" role="navigation">
-            <ul id="menu-gd-menu" class="menu">
-                @guest
-                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('login') }}">Login</a></li>
-                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('register') }}">Register</a></li>
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Roboto');
+        body{
+            font-family: 'Roboto', sans-serif;
+        }
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        i {
+            margin-right: 10px;
+        }
+        /*----------multi-level-accordian-menu------------*/
+        .navbar-logo{
+            padding: 15px;
+            color: #fff;
+        }
+        .navbar-mainbg{
+            background-color: #292929;
+            padding: 0px;
+        }
+        #navbarSupportedContent{
+            overflow: hidden;
+            position: relative;
+        }
+        #navbarSupportedContent ul{
+            padding: 0px;
+            margin: 0px;
+        }
+        #navbarSupportedContent ul li a i{
+            margin-right: 10px;
+        }
+        #navbarSupportedContent li {
+            list-style-type: none;
+            float: left;
+        }
+        #navbarSupportedContent ul li a{
+            color: rgba(255,255,255,0.5);
+            text-decoration: none;
+            font-size: 15px;
+            display: block;
+            padding: 20px 20px;
+            transition-duration:0.6s;
+            transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
+            position: relative;
+        }
+        #navbarSupportedContent>ul>li.active>a{
+            color: #5161ce;
+            background-color: transparent;
+            transition: all 0.7s;
+        }
+        #navbarSupportedContent a:not(:only-child):after {
+            content: "\f105";
+            position: absolute;
+            right: 20px;
+            top: 10px;
+            font-size: 14px;
+            font-family: "Font Awesome 5 Free";
+            display: inline-block;
+            padding-right: 3px;
+            vertical-align: middle;
+            font-weight: 900;
+            transition: 0.5s;
+        }
+        #navbarSupportedContent .active>a:not(:only-child):after {
+            transform: rotate(90deg);
+        }
+        .hori-selector{
+            display:inline-block;
+            position:absolute;
+            height: 100%;
+            top: 0px;
+            left: 0px;
+            transition-duration:0.6s;
+            transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
+            background-color: #fff;
+            border-top-left-radius: 15px;
+            border-top-right-radius: 15px;
+            margin-top: 10px;
+        }
+        .hori-selector .right,
+        .hori-selector .left{
+            position: absolute;
+            width: 25px;
+            height: 25px;
+            background-color: #292929;
+            bottom: 10px;
+        }
+        .hori-selector .right{
+            right: -25px;
+        }
+        .hori-selector .left{
+            left: -25px;
+        }
+        .hori-selector .right:before,
+        .hori-selector .left:before{
+            content: '';
+            position: absolute;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background-color: #292929;
+        }
+        .hori-selector .right:before{
+            bottom: 0;
+            right: -25px;
+        }
+        .hori-selector .left:before{
+            bottom: 0;
+            left: -25px;
+        }
+        @media (max-width: 991px){
+            #navbarSupportedContent ul li a{
+                padding: 12px 30px;
+            }
+            .hori-selector{
+                margin-top: 0px;
+                margin-left: 10px;
+                border-radius: 0;
+                border-top-left-radius: 25px;
+                border-bottom-left-radius: 25px;
+            }
+            .hori-selector .left,
+            .hori-selector .right{
+                right: 10px;
+            }
+            .hori-selector .left{
+                top: -25px;
+                left: auto;
+            }
+            .hori-selector .right{
+                bottom: -25px;
+            }
+            .hori-selector .left:before{
+                left: -25px;
+                top: -25px;
+            }
+            .hori-selector .right:before{
+                bottom: -25px;
+                left: -25px;
+            }
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-mainbg navbar-dark">
+    <a class="navbar-brand" href="/">Iktakhairul</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
+            @if (Route::has('login'))
+                @auth
+                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('admin.shops.index') }}">Dashboard</a></li>
                 @else
-                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('admin.shops.index') }}">Manage Shops</a></li>
-                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="#" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">Logout</a></li>
-                    <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                @endguest
-            </ul>
-        </nav>
-        <div class="dt-nav-toggle  dt-mobile-nav-button-wrap"><a href="#primary-nav"><i class="fas fa-bars"></i></a></div>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}"><i class="far"></i>Login</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}"><i class="far"></i>Register</a>
+                        </li>
+                    @endif
+                @endauth
+            @endif
+        </ul>
+        <ul id="menu-gd-menu" class="menu">
+            @guest
+                <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('login') }}">Login</a></li>
+                <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('register') }}">Register</a></li>
+            @else
+                <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="#" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">Logout</a></li>
+                <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            @endguest
+        </ul>
     </div>
-</header>
+</nav>
+<script>
+</script>
+<div class="container">
+</div>
+<div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+</html>
